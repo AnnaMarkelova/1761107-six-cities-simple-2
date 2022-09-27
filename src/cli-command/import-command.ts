578b448a0709e544a24bae65925c4a882ed6f468
Anna Markelova <anytkamarkelova@yandex.ru>
2022-09-27 18:32:43 +0200

    adds record data to DB

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -1,28 +1,82 @@
 import chalk from 'chalk';
+import { DatabaseInterface } from '../common/database-client/database.interface.js';
+import DatabaseService from '../common/database-client/database.service.js';
 import TSVFileReader from '../common/file-reader/tsv-file-reader.js';
+import ConsoleLoggerService from '../common/logger/console-logger.service.js';
+import { LoggerInterface } from '../common/logger/logger.interface.js';
+import { OfferServiceInterface } from '../modules/offer/offer-service.interface.js';
+import { OfferModel } from '../modules/offer/offer.entity.js';
+import OfferService from '../modules/offer/offer.service.js';
+import { UserServiceInterface } from '../modules/user/user-service.interface.js';
+import { UserModel } from '../modules/user/user.entity.js';
+import UserService from '../modules/user/user.service.js';
+import { Hotel } from '../types/hotel.type.js';
 import { createHotel, getErrorMessage } from '../utils/common.js';
+import { getURI } from '../utils/db.js';
 import { CliCommandInterface } from './cli-command.interface.js';
+
+const DEFAULT_DB_PORT = 27017;
+const DEFAULT_USER_PASSWORD = '123456';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
+  private userService!: UserServiceInterface;
+  private offerService!: OfferServiceInterface;
+  private databaseService!: DatabaseInterface;
+  private logger: LoggerInterface;
+  private salt!: string;
 
-  private onLine(line: string) {
-    const offer = createHotel(line);
-    console.log(chalk.bgBlue(JSON.stringify(offer)));
+  constructor() {
+    this.onLine = this.onLine.bind(this);
+    this.onComplete = this.onComplete.bind(this);
+
+    this.logger = new ConsoleLoggerService();
+    this.offerService = new OfferService(this.logger, OfferModel);
+    this.userService = new UserService(this.logger, UserModel);
+    this.databaseService = new DatabaseService(this.logger);
   }
+
+  private async saveOffer(offer: Hotel) {
+    const user = await this.userService.findOrCreate({
+      ...offer.host,
+      password: DEFAULT_USER_PASSWORD
+    }, this.salt);
+
+    await this.offerService.create({
+      ...offer,
+      host: user.id,
+    });
+  }
+
+  private async onLine(line: string, resolve: () => void) {
+    const offer = createHotel(line);
+    await this.saveOffer(offer);
+    resolve();
+  }
+
+  // private onLine(line: string) {
+  //   const offer = createHotel(line);
+  //   console.log(chalk.bgBlue(JSON.stringify(offer)));
+  // }
 
   private onComplete(count: number) {
     console.log(chalk.redBright(`${count} rows imported.`));
+    this.databaseService.disconnect();
   }
 
-  public async execute(filename: string): Promise<void> {
+  public async execute(filename: string, login: string, password: string, host: string, dbname: string, salt: string): Promise<void> {
+    const uri = getURI(login, password, host, DEFAULT_DB_PORT, dbname);
+    this.salt = salt;
+
+    await this.databaseService.connect(uri);
+
     const fileReader = new TSVFileReader(filename.trim());
     fileReader.on('line', this.onLine);
     fileReader.on('end', this.onComplete);
 
     try {
       await fileReader.read();
-    } catch(err) {
+    } catch (err) {
       console.log(`Can't read the file: ${getErrorMessage(err)}`);
     }
   }
