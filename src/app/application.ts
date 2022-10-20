f4f24c2a874ef5b5db5ca0d9da26ad3360c1d729
Anna Markelova <anytkamarkelova@yandex.ru>
2022-10-20 12:44:06 +0200

    creates Express

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,20 +1,25 @@
 import { inject, injectable } from 'inversify';
+import express, {Express} from 'express';
 import { ConfigInterface } from '../common/config/config.interface.js';
 import { DatabaseInterface } from '../common/database-client/database.interface.js';
 import { LoggerInterface } from '../common/logger/logger.interface.js';
-import { OfferServiceInterface } from '../modules/offer/offer-service.interface.js';
+// import { OfferServiceInterface } from '../modules/offer/offer-service.interface.js';
 import { Component } from '../types/component.types.js';
 import { getURI } from '../utils/db.js';
 
 @injectable()
 export default class Application {
 
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
-    @inject(Component.OfferServiceInterface) private offerService: OfferServiceInterface
-  ) {}
+    // @inject(Component.OfferServiceInterface) private offerService: OfferServiceInterface
+  ) {
+    this.expressApp = express();
+  }
 
   public async init() {
     this.logger.info('Application initialization…');
@@ -36,5 +41,8 @@ export default class Application {
     //const offers = await this.offerService.calcRating('63358d9850bcefa5f37e2e5d', 2);
     // const offers = await this.offerService.findById('63358d9850bcefa5f37e2e5d');
     // console.log(offers);
+
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
