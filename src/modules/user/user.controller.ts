29ef2adb8904a25f8b8f6308f90b8ac7b373f6cc
Anna Markelova <anytkamarkelova@yandex.ru>
2022-10-21 12:40:38 +0200

    adds login in User controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -11,6 +11,7 @@ import { fillDTO } from '../../utils/common.js';
 import UserResponse from './response/user.response.js';
 import { ConfigInterface } from '../../common/config/config.interface.js';
 import HttpError from '../../common/errors/http-error.js';
+import LoginUserDto from './dto/login-user.dto.js';
 
 @injectable()
 export default class UserController extends Controller {
@@ -24,6 +25,7 @@ export default class UserController extends Controller {
     this.logger.info('Register routes for UserController...');
 
     this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({path: '/login', method: HttpMethod.Post, handler: this.login});
   }
 
   public async create(
@@ -42,5 +44,25 @@ export default class UserController extends Controller {
     const result = await this.userService.create(body, this.config.get('SALT'));
 
     this.created(res, fillDTO(UserResponse, result));
+  }
+
+  public async login(
+    { body }: Request<Record<string, unknown>, Record<string, unknown>, LoginUserDto>,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (!existsUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} not found.`,
+        'UserController',
+      );
+    }
+
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController',
+    );
   }
 }
