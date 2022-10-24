403e0bd6eedab03270ab661b321e98b8a068c5db
Anna Markelova <anytkamarkelova@yandex.ru>
2022-10-24 14:20:51 +0200

    adds user validation

--- src/modules/offer/dto/create-offer.dto.ts
+++ src/modules/offer/dto/create-offer.dto.ts
@@ -18,7 +18,7 @@ import { City } from '../../../types/city.type.js';
 import { GoodType } from '../../../types/good-type.enum.js';
 import { HotelType } from '../../../types/hotel-type.enum.js';
 import { Location } from '../../../types/location.type.js';
-import { ADULTS_MAX, ADULTS_MIN, BEDROOMS_MAX, BEDROOMS_MIN, DESCRIPTION_LENGTH_MAX, DESCRIPTION_LENGTH_MIN, FILENAME_LENGTH, IMAGES_COUNT, PRICE_MAX, PRICE_MIN, TITLE_LENGTH_MAX, TITLE_LENGTH_MIN } from '../const.js';
+import { ADULTS_MAX, ADULTS_MIN, BEDROOMS_MAX, BEDROOMS_MIN, DESCRIPTION_LENGTH_MAX, DESCRIPTION_LENGTH_MIN, FILENAME_LENGTH, IMAGES_COUNT, PRICE_MAX, PRICE_MIN, TITLE_LENGTH_MAX, TITLE_LENGTH_MIN } from '../consts.js';
 
 export default class CreateOfferDto {
 
--- src/modules/user/dto/login-user.dto.ts
+++ src/modules/user/dto/login-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+import { PASSWORD_LENGTH_MAX, PASSWORD_LENGTH_MIN } from '../consts.js';
 export default class LoginUserDto {
+  @IsString({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Email must be valid email address' })
   public email!: string;
+
+  @IsString({ message: 'password is required' })
+  @Length(PASSWORD_LENGTH_MIN, PASSWORD_LENGTH_MAX, {message: `Min length for password is ${PASSWORD_LENGTH_MIN}, max is ${PASSWORD_LENGTH_MAX}`})
   public password!: string;
 }
