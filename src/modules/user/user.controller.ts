591916e94881eff97e7ecf9e842755adb70d230b
Anna Markelova <anytkamarkelova@yandex.ru>
2022-10-25 14:00:36 +0200

    adds checkAuthenticate

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -49,6 +49,11 @@ export default class UserController extends Controller {
         new UploadFileMiddleware(this.config.get('UPLOAD_DIRECTORY'), 'avatar'),
       ]
     });
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Get,
+      handler: this.checkAuthenticate
+    });
   }
 
   public async create(
@@ -97,5 +102,10 @@ export default class UserController extends Controller {
     this.created(res, {
       filepath: req.file?.path
     });
+  }
+
+  public async checkAuthenticate(req: Request, res: Response) {
+    const user = await this.userService.findByEmail(req.user.email);
+    this.ok(res, fillDTO(LoggedUserResponse, user));
   }
 }
